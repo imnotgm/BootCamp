98b695b3451c1adf38368fb495653ab76b5d6fae
kmin <iamlkm0810@gmail.com>
2024-08-30 15:31:38 +0900

    problem solved: error check updated\n\n-termination after 3 tries\n-negative number functions propely

--- .github/workflows/numFormat.cpp
+++ .github/workflows/numFormat.cpp
@@ -5,22 +5,37 @@ using namespace std;
 
 int main()
 {
-    string N; int nlength, trial = 0;
+    string N; int nlength, trial = 0; bool flag=false;
     while(true){
         cin >> N;
         nlength = N.length();
         for (int i=0; i<nlength; i++){
-            if(N[i]<=9 && N[i]>=0)
+            if((N[i]<= '9' && N[i]>='0') || N[0]=='-')
                 continue;
             else{
                 trial++;
+                flag=true;
                 break;
             }
         }
         if (trial == 3){
-            printf("ERROR! OUT OF TRIAL");
+            printf("There's one or more character which is not a number\nERROR! OUT OF TRIAL");
             trial = 0;
             break;
+        }
+        if(flag == true){
+            cout << "There's one or more character which is not a number" << endl;
+            flag=false;
+            continue;
+        }
+        if(N[0] == '-'){
+            cout<< '-' ;
+            N = N.substr(1, N.length());
+            nlength= N.length();
+        }
+        if(nlength>20){
+            cout << "N is more than 20 character" << endl;
+            continue;
         }
         for(int i=0; i<nlength; i++){
             if (i%3 == nlength%3 && i!=0)
